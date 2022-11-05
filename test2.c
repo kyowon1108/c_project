13445358e2ba57bace08f6c08af1171caeb5de03
kapr <kyowon1108@gmail.com>
2022-11-05 18:00:59 +0900

    asdf

--- test2.c
+++ test2.c
@@ -3,38 +3,31 @@
 #include <stdio.h>
 
 
-#define DB_HOST "localhost"
-#define DB_USER "kapr"
-#define DB_PASS "kyowon1108"
-#define DB_NAME "PFP"
+#define HOST "localhost"
+#define USER "kapr"
+#define PASSWORD "kyowon1108"
+#define DB "PFP"
 #define CHOP(x) x[strlen(x) - 1] = ' '
     
-int main(void)
-{
-    MYSQL       *connection=NULL, conn;
-    MYSQL_RES   *sql_result;
-    MYSQL_ROW   sql_row;
-    int       query_stat; 
+int main(void) {
+    MYSQL *connection=NULL, conn;
+    MYSQL_RES *sql_result;
+    MYSQL_ROW sql_row;
+    int query_stat; 
 
-    char name[12];
-    char address[80];
-    char tel[12];
-    char query[255];
+    char query[255]; // 입력할 mysql 쿼리문이 들어갈 변수
     
     mysql_init(&conn);
 
-    connection = mysql_real_connect(&conn, DB_HOST,
-                                    DB_USER, DB_PASS,
-                                    DB_NAME, 3306,
-                                    (char *)NULL, 0);
+    connection = mysql_real_connect(&conn, HOST, USER, PASSWORD, DB, 3306, NULL, 0); // 서버와 통신 시작
 
-    if (connection == NULL)
+    if (connection == NULL) // 연결 실패시
     {
         fprintf(stderr, "Mysql connection error : %s", mysql_error(&conn));
         return 1;
     }
 
-    query_stat = mysql_query(connection, "select * from User");
+    query_stat = mysql_query(connection, "select * from User"); 
     if (query_stat != 0)
     {
         fprintf(stderr, "Mysql query error : %s", mysql_error(&conn));
@@ -42,22 +35,19 @@ int main(void)
     }
     
     sql_result = mysql_store_result(connection);
-    
-    // printf("%+11s %-30s %-10s", "이름", "주소", "전화번호");
-    // while ( (sql_row = mysql_fetch_row(sql_result)) != NULL )
-    // {
-    //     printf("%+11s %-30s %-10s", sql_row[0], sql_row[1], sql_row[2]);
-    // }
+    printf("--------------------------------------");
+    while ( (sql_row = mysql_fetch_row(sql_result)) != NULL ) {
+        printf("%+11s %-30s", sql_row[0], sql_row[1]);
+    }
+    printf("--------------------------------------");
 
     mysql_free_result(sql_result);
 
-    printf("이름 :");
+    printf("name :");
     fgets(name, 12, stdin);
     CHOP(name);
 
-    sprintf(query, "insert into User values "
-                   "(1, '%s')",
-                   name);
+    sprintf(query, "insert into User values (0, '%s')", name); // User을 추가하는 명령어
 
     query_stat = mysql_query(connection, query);
     if (query_stat != 0)
