94046349ea816f98ddf4036652173b186ed9cea7
kapr <kyowon1108@gmail.com>
2022-11-05 20:12:07 +0900

    asd

--- test2.c
+++ test2.c
@@ -8,8 +8,7 @@
 #define PASSWORD "kyowon1108"
 #define DB "PFP"
 #define CHOP(x) x[strlen(x) - 1] = ' '
-
-
+    
 int main(void) {
     MYSQL *connection = NULL, conn;
     MYSQL_RES *sql_result;
@@ -22,11 +21,11 @@ int main(void) {
 
     connection = mysql_real_connect(&conn, HOST, USER, PASSWORD, DB, 3306, NULL, 0); // 서버와 통신 시작
 
-    // if (connection == NULL) // 연결 실패시
-    // {
-    //     fprintf(stderr, "Mysql connection error : %s", mysql_error(&conn));
-    //     return 1;
-    // }
+    if (connection == NULL) // 연결 실패시
+    {
+        fprintf(stderr, "Mysql connection error : %s", mysql_error(&conn));
+        return 1;
+    }
 
     query_stat = mysql_query(connection, "select * from User"); // 쿼리문이 성공할 경우에는 0을 출력함
     if (query_stat != 0)
@@ -36,16 +35,40 @@ int main(void) {
     }
     
     sql_result = mysql_store_result(connection);
-    printf("--------------------------------------\n");
+    // printf("--------------------------------------\n");
+    // while ( (sql_row = mysql_fetch_row(sql_result)) != NULL ) {
+    //     printf("%s : %s\n", sql_row[0], sql_row[1]);
+    // }
+    // printf("--------------------------------------\n\n");
+
+    mysql_free_result(sql_result);
+
+    char name[30];
+    printf("name :");
+    fgets(name, 12, stdin);
+
+    sprintf(query, "insert into User values (0, '%s')", name); // User을 추가하는 명령어
+
+    query_stat = mysql_query(connection, query);
+    if (query_stat != 0)
+    {
+        fprintf(stderr, "Mysql query error : %s", mysql_error(&conn));
+        return 1;
+    }
+
+    query_stat = mysql_query(connection, "select * from User"); 
+    if (query_stat != 0)
+    {
+        fprintf(stderr, "Mysql query error : %s", mysql_error(&conn));
+        return 1;
+    }
+    
+    sql_result = mysql_store_result(connection);
+    printf("\n--------------------------------------\n");
     while ( (sql_row = mysql_fetch_row(sql_result)) != NULL ) {
         printf("%s : %s\n", sql_row[0], sql_row[1]);
     }
     printf("--------------------------------------\n\n");
 
-
-
-    mysql_free_result(sql_result);
     mysql_close(connection);
-
-    return 0;
 }
