45543b1d5d02b5ed5f17fde5f07efe088dd6b480
kapr <kyowon1108@gmail.com>
2022-11-07 15:38:51 +0900

    asd

--- test2.c
+++ test2.c
@@ -110,7 +110,7 @@ int GetPlanLen(int userIdx) {
     sql_result = mysql_store_result(connection);
     char * res;
     while ( (sql_row = mysql_fetch_row(sql_result)) != NULL ) res = sql_row[0];
-    return (int)res;
+    return res - '0';
 }
 
 int GetPlan(int userIdx, plan pl) {
