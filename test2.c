0209fa97f994cdfa468374b375fd48fdc747328d
kapr <kyowon1108@gmail.com>
2022-11-10 15:35:38 +0900

    asd

--- test2.c
+++ test2.c
@@ -132,7 +132,7 @@ int GetPlan(int userIdx) {
     
     sql_result = mysql_store_result(connection);
     int len = GetPlanLen(userIdx);
-    char arr[10];
+    char** arr[10];
     while ( (sql_row = mysql_fetch_row(sql_result)) != NULL ) {
         for(int i = 0; i < len; ++i) {
             arr[i] = sql_row;
