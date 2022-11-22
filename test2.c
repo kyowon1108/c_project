9618b0b3d8ce51ec3edd11f49d82be4e2dd7b17a
kapr <kyowon1108@gmail.com>
2022-11-22 17:25:51 +0900

    asd

--- test2.c
+++ test2.c
@@ -403,21 +403,21 @@ int main(void) {
                     printf("Friend's plan does not exist. Return to the number selection window.\n\n");
                     break;
                 }
-                free(idxArr), free(nameArr);
-                idxArr = (int*)malloc(sizeof(int) * planLen);
-                nameArr = (char**)malloc(sizeof(char*) * planLen);
-                for(int i = 0; i < planLen; ++i) {
-                    *(nameArr+i) = (char*)malloc(sizeof(char) * 20);
-                }
-                char ** endArr = (char**)malloc(sizeof(char*) * planLen);
-                for(int i = 0; i < planLen; ++i) {
-                    *(endArr+i) = (char*)malloc(sizeof(char) * 20);
-                } 
-                GetFriendPlan(userIdx, friendIdx, idxArr, nameArr, endArr);
-                for (int i = 0; i < planLen; ++i) {
-                    printf("%d, %s, %s\n", *(idxArr + i), *(nameArr + i), *(endArr + i));
-                }
-                free(idxArr), free(nameArr), free(endArr);
+                // free(idxArr), free(nameArr);
+                // idxArr = (int*)malloc(sizeof(int) * planLen);
+                // nameArr = (char**)malloc(sizeof(char*) * planLen);
+                // for(int i = 0; i < planLen; ++i) {
+                //     *(nameArr+i) = (char*)malloc(sizeof(char) * 20);
+                // }
+                // char ** endArr = (char**)malloc(sizeof(char*) * planLen);
+                // for(int i = 0; i < planLen; ++i) {
+                //     *(endArr+i) = (char*)malloc(sizeof(char) * 20);
+                // } 
+                // GetFriendPlan(userIdx, friendIdx, idxArr, nameArr, endArr);
+                // for (int i = 0; i < planLen; ++i) {
+                //     printf("%d, %s, %s\n", *(idxArr + i), *(nameArr + i), *(endArr + i));
+                // }
+                // free(idxArr), free(nameArr), free(endArr);
                 break; }
 
             case 7 : {
