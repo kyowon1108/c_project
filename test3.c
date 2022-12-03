963121b838e61331d0f874ed37044867e0d2d0d8
kapr <kyowon1108@gmail.com>
2022-12-03 20:04:46 +0900

    asd

--- test3.c
+++ test3.c
@@ -113,7 +113,7 @@ int main(void) {
                             t->tm_year + 1900, t->tm_mon + 1, t->tm_mday,
                             t->tm_hour, t->tm_min, t->tm_sec
                             );
-                    sprintf(log, "[%s] %d : %s\n", time, userIdx, content);
+                    sprintf(log, "[%s] %d : %s\n", time, userIdx, "content");
                     fputs(log, fp);
                     fclose(fp);
                     //MakeLog(userIdx, "signin");
